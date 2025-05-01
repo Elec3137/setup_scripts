70c36ce6b8cb3d96b126449e9774de766a4dfbd0
Elec3137 <elec3137@proton.me>
2025-05-01 11:04:06 -0700

    fix up maliit script, avoid deleting the original binary when reran

--- maliit-dark.sh
+++ maliit-dark.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # creates a bash script as /bin/maliit-keyboard to set env variables as it starts
 
+[ -f /bin/maliit-keyboard_og ] && printf "ALREADY APPLIED\n" && exit
+
 sudo mv /bin/maliit-keyboard /bin/maliit-keyboard_og || exit
-echo '''
-#!/bin/sh
+echo '#!/bin/sh
 export QT_QUICK_CONTROLS_STYLE=Universal
 export QT_QUICK_CONTROLS_UNIVERSAL_THEME=Dark
-/bin/maliit-keyboard_og''' | sudo tee /bin/maliit-keyboard
+/bin/maliit-keyboard_og' | sudo tee /bin/maliit-keyboard
 sudo chmod +x /bin/maliit-keyboard
